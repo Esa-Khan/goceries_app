461d3a52afcafab6c4fc997dd2213012b287b553
Esa Khan <esa.khan28@gmail.com>
2020-06-16 12:11:47 +0500

    Added new right icon

--- build-apk.sh
+++ build-apk.sh
@@ -7,4 +7,4 @@ flutter pub get
 flutter build apk --release
 
 # move file app-release.apk to root folder
-cp "$PATH_PROJECT/build/app/outputs/apk/release/app-release.apk" "$PATH_PROJECT/multi_restaurants_flutter_app.apk"
+cp "$PATH_PROJECT/build/app/outputs/apk/release/app-release.apk" "$PATH_PROJECT/goceries.apk"
