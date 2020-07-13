6603cf5050f0444911435c24d28b569390834505
Esa Khan <esa.khan28@gmail.com>
2020-07-13 19:55:45 +0500

    Merged iOS implementation

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
-
+    GMSServices.provideAPIKey("AIzaSyCVuBvhnt9LE277eSTwAu--tfiIBu9-b10")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
